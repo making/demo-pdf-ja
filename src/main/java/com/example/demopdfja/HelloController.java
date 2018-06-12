0021a10ad4caa9b7c9b65f025bec06d1502c943c
Toshiaki Maki <tmaki@pivotal.io>
2018-06-12 21:38:00 +0900

    Fix path attributes

--- src/main/java/com/example/demopdfja/HelloController.java
+++ src/main/java/com/example/demopdfja/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.demopdfja;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping(path = "hello.pdf", produces = MediaType.APPLICATION_PDF_VALUE)
     public String hello(Model model) {
         model.addAttribute("message", "こんにちは!");
         return "hello";
